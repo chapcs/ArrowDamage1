13ed3063f4be6cba84e87bccc57a6d271f4d04c2
chapcs <benchapman003@gmail.com>
2023-04-08 11:01:55 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+namespace ArrowDamage1
+{
+    class ArrowDamage
+    {
+        private const decimal BASE_MULTIPLIER = 0.35M;
+        private const decimal MAGIC_MULTIPLIER = 2.5M;
+        private const decimal FLAME_DAMAGE = 1.25M;
+
+        public int roll;
+        public bool flaming;
+        public bool magic;
+
+        public int Damage { get; private set; }
+
+        public ArrowDamage(int startingRoll)
+        {
+            roll = startingRoll;
+            CalculateDamage();
+        }
+
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+
+        private void CalculateDamage()
+        {
+            decimal baseDamage = Roll * BASE_MULTIPLIER;
+            if (Magic) baseDamage *= MAGIC_MULTIPLIER;
+            if (Flaming) baseDamage += FLAME_DAMAGE;
+            Damage = (int)Math.Ceiling(baseDamage);
+
+            Debug.WriteLine($"CalculateDamage finished: {Damage} (roll: {Roll})");
+        }
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+    }
+}
---
+++ SwordDamage.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Main method that calculates the the sword damage based off different params
+/// </summary>
+namespace SwordDamage
+{
+    class SwordDamage
+    {
+        public const int BASE_DAM = 3;
+        public const int FLAME_DAM = 2;
+
+        public int roll;
+        public bool flaming;
+        public bool magic;
+
+        public int Damage { get; private set; }
+
+        public SwordDamage(int startingRoll)
+        {
+            roll = startingRoll;
+            CalculateDamage();
+        }
+
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+
+        private void CalculateDamage()
+        {
+            decimal MagicMult = 1M;
+            if (Magic) MagicMult = 1.75M;
+
+            Damage = (int)(Roll * MagicMult) + BASE_DAM;
+            if (Flaming) Damage += FLAME_DAM;
+            Debug.WriteLine($"CalculateDamage finished: {Damage} (roll: {Roll})");
+        }
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+    }
+}
