f31fcd39f596d7f2b6671d72930235f020057bc6
chapcs <benchapman003@gmail.com>
2023-04-20 07:41:02 -0400

    Added main method to be altered

--- Program.cs
+++ Program.cs
@@ -1,11 +1,52 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using SwordDamage;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("\nS for sword, A for arrow, anything else to quit: ");
+        char weaponKey = Char.ToUpper(Console.ReadKey().KeyChar);
+        // could use some if/else statements to pick between the two characters
+        // instead we will use a switch statement designed to do this
+        switch (weaponKey)
+        {
+            case 'S':
+                /* calculate sword damage */
+                break;
+            case 'A':
+                /* calculate arrow damage */
+                break;
+            default:
+                return;
+        }
+    }
+    private static void Main(string[] args)
+    {
+        SwordDamage sword = new SwordDamage(RollDice());
+
+        while (true)
+        {
+            Console.Write("0 for no abilities, 1 for magic, 2 for flaming, 3 for both; press any other key to quit: ");
+            char keyChar = Console.ReadKey().KeyChar; // the boolean in this line determines whether to intercept the character, optional to display in console; KeyChar selects the character to be stored in keyChar; ReadKey() defaults to false
+
+            if (keyChar == '0' || keyChar == '1' || keyChar == '2' || keyChar == '3')
+            {
+                sword.Roll = RollDice();
+                sword.Magic = (keyChar == '1' || keyChar == '3');
+                sword.Flaming = (keyChar == '2' || keyChar == '3');
+                Console.WriteLine($"\nRolled {sword.Roll} for {sword.Damage} HP \n");
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+    static int RollDice()
+    {
+        return random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
     }
 }
 
